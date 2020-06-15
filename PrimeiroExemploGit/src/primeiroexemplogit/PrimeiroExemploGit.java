31a6c380832b9b36420cd3e9dbcc57d229c8d7d2
mpisching <mpisching@gmail.com>
2020-06-15 19:05:24 -0300

    Projeto netbeans - exemplo Git

---
+++ PrimeiroExemploGit/src/primeiroexemplogit/PrimeiroExemploGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiroexemplogit;
+
+/**
+ *
+ * @author mpisching
+ */
+public class PrimeiroExemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Experimentando um repositório Git");
+    }
+    
+}
